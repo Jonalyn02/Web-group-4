417ceb34367a4c28c37630ba28be46bc2917b9b0
Jonalyn02 <gubataojonalyn@gmail.com>
2024-09-04 02:29:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="style.css">
+    <title>Team Profile | Group 4</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-container">
+            <div class="logo" data-aos="zoom-in" data-aos-duration="1500">
+                GROUP<span>4</span>
+            </div>
+            <div class="links">
+                <div class="link" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="100"><a href="#">Home</a></div>
+                <div class="link" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="200"><a href="#">About</a></div>
+        </div>
+        <i class="fa-solid fa-bars hamburg" onclick="hamburg()"></i>
+        <div class="dropdown">
+            <div class="links">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <i class="fa-solid fa-xmark cancel" onclick="cancel()"></i>
+            </div>
+        </div>
+    </nav>
+    <section class="home" id="home">
+        <div class="main-container">
+            <div class="content">
+                <h1 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="700">Hey I'm <span>jonalyn Gubatao</span></h1>
+                <div data-aos="fade-right" data-aos-duration="1500" data-aos-delay="900" class="typewriter">I'm a <span class="typewriter-text"></span><label for="">|</label></div>
+                <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">Excels as our Coder and UI/UX Designer, seamlessly blending technical expertice with a strong sense if design she crafts intuative user interfaces and ensures a smooth, engaging user experience, while her coding skills bring these designs to life with precision and efficiency </p>
+                <div class="social-links">
+                    <a data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1300"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1400"><i class="fa-brands fa-github"></i></a>
+                    <a href="https://www.facebook.com/delacruz.jonalyn.2002" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1600"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+                <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                    <button>More About Me</button>
+                </div>
+            </div>
+            <div class="image" data-aos="zoom-in" data-aos-duration="3000">
+                <img src="jona.jfif" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="about" id="about">
+
+        <div class="about-img" data-aos="zoom-in" data-aos-duration="3000">
+            <img src="jona.jfif">
+        </div>
+
+        <div class="about-content">
+            <h2>About <span>Me</span></h2>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">QPAL KA BA BOSSING!!!!!!!</p>
+            <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                <button>Read More</button>
+            </div>
+
+    </section>
+
+
+
+<section>
+        <div class="main-container">
+            <div class="content">
+                <h1 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="700">Hey I'm <span>George Gabriel Manlangit</span></h1>
+                <div data-aos="fade-right" data-aos-duration="1500" data-aos-delay="900" class="typewriter">I'm a <span class="typewriter-text"></span><label for="">|</label></div>
+                <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">Is a skilled Programmer and Designer, proficient in languages like Python and JavaScriot, and experience in web developmenet with HTML, CSS, React, and Angular. He excel in UI/UX design using tools like Adobe Creative suite and Figma. George blends technical expertice with creative insight to deliver seamless, user-friendly solutions and committed to continuous learning and innovation. </p>
+                <div class="social-links">
+                    <a data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1300"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1400"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1600"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+                <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                    <button>Download CV</button>
+                </div>
+            </div>
+            <div class="image" data-aos="zoom-in" data-aos-duration="3000">
+                <img src="jabby.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="about" id="about">
+
+        <div class="about-img" data-aos="zoom-in" data-aos-duration="3000">
+            <img src="jabby.jpg">
+        </div>
+
+        <div class="about-content">
+            <h2>About <span>Me</span></h2>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">I'm IT Student 3rd Year Section M From Pamantasan ng Lungsod ng Muntinlupa(Plmun).</p>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">And I'm from Parañaque City.</p>
+            <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                <button>Read More</button>
+            </div>
+
+    </section>
+
+    <section>
+        <div class="main-container">
+            <div class="content">
+                <h1 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="700">Hey I'm <span>Vincent Galgana</span></h1>
+                <div data-aos="fade-right" data-aos-duration="1500" data-aos-delay="900" class="typewriter">I'm a <span class="typewriter-text"></span><label for="">|</label></div>
+                <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">Leader and Editor, known for his strategic vision and meticulous attention to detail. He excels in refining content and guiding the team to achieve high-quality results while fostering a collaborate environment.</p>
+                <div class="social-links">
+                    <a data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1300"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1400"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1600"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+                <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                    <button>Download CV</button>
+                </div>
+            </div>
+            <div class="image" data-aos="zoom-in" data-aos-duration="3000">
+                <img src="vincent.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="about" id="about">
+
+        <div class="about-img" data-aos="zoom-in" data-aos-duration="3000">
+            <img src="vincent.jpg">
+        </div>
+
+        <div class="about-content">
+            <h2>About <span>Me</span></h2>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100"></p>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">
+                <p  data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">fostering a collaborate environment</p>
+            </p>
+            <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                <button>Read More</button>
+            </div>
+
+    </section>
+
+    <section>
+        <div class="main-container">
+            <div class="content">
+                <h1 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="700">Hey I'm <span>Allen Conchada</span></h1>
+                <div data-aos="fade-right" data-aos-duration="1500" data-aos-delay="900" class="typewriter">I'm a <span class="typewriter-text"></span><label for="">|</label></div>
+                <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">Supports our development efforts as an Assistant coder, providing crucial assistance in writing, testing and debugging code. His attention to detail and provlem-solving skills help streamline our coding processes and contribute to the overall success of our projects.</p>
+                <div class="social-links">
+                    <a data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1300"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1400"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1600"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+                <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                    <button>Download CV</button>
+                </div>
+            </div>
+            <div class="image" data-aos="zoom-in" data-aos-duration="3000">
+                <img src="allen.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="about" id="about">
+
+        <div class="about-img" data-aos="zoom-in" data-aos-duration="3000">
+            <img src="allen.jpg">
+        </div>
+
+        <div class="about-content">
+            <h2>About <span>Me</span></h2>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">QPAL KA BA BOSSING!!!!!!!</p>
+            <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                <button>Read More</button>
+            </div>
+
+    </section>
+
+    <section>
+        <div class="main-container">
+            <div class="content">
+                <h1 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="700">Hey I'm <span>Cromwell Carmelina</span></h1>
+                <div data-aos="fade-right" data-aos-duration="1500" data-aos-delay="900" class="typewriter">I'm a <span class="typewriter-text"></span><label for="">|</label></div>
+                <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">Skilled UI/UX designer with a knack for blending aesthetics and functionality. His design approach is driven by user research and interative testing, ensring intuative and engaging interfaces. Darwin's collaborative nature and industry experience allow him to align design strategies with project goals effectively, making him a valuable asset in delivering impactful user experiences </p>
+                <div class="social-links">
+                    <a data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1300"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1400"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1600"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+                <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                    <button>Download CV</button>
+                </div>
+            </div>
+            <div class="image" data-aos="zoom-in" data-aos-duration="3000">
+                <img src="cwell.png">
+            </div>
+        </div>
+    </section>
+
+    <section class="about" id="about">
+
+        <div class="about-img" data-aos="zoom-in" data-aos-duration="3000">
+            <img src="cwell.png">
+        </div>
+
+        <div class="about-content">
+            <h2>About <span>Me</span></h2>
+            <p data-aos="flip-down" data-aos-duration="1500" data-aos-delay="1100">QPAL KA BA BOSSING!!!!!!!</p>
+            <div class="btn" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1800">
+                <button>Read More</button>
+            </div>
+
+    </section>
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init({offset:0,});
+    </script>
+    <script src="main.js"></script>
+    
+</body>
+</html>
